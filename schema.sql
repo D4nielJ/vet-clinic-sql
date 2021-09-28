5af734b45d2684b3added9a63b955a726084ee61
d4nielj <d4niel.djm@gmail.com>
2021-09-28 09:46:19 -0500

    Fix Schema.sql

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 /* Database schema to keep the structure of entire database. */
 
 CREATE
-TABLE public.animals
+TABLE animals
 (
     id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name VARCHAR(100),
@@ -14,13 +14,13 @@ TABLE public.animals
 -- Second Milestone
 
 ALTER TABLE
-public.animals 
+animals 
 ADD COLUMN IF NOT EXISTS species VARCHAR(100); 
 
 -- Third Milestone
 
 CREATE
-TABLE public.owners
+TABLE owners
 (
     id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     full_name VARCHAR(100),
@@ -28,26 +28,24 @@ TABLE public.owners
 );
 
 CREATE
-TABLE public.species (
+TABLE species (
     id int NOT NULL GENERATED ALWAYS AS IDENTITY,
     name varchar(100);
 );
 
 ALTER TABLE
-public.animals
+animals
 DROP COLUMN species;
 
-ALTER TABLE
-public.animals
-ADD COLUMN IF NOT EXISTS species_id INT REFERENCES public.species(id);
+ALTER TABLE animals add column species_id int NULL;
+ALTER TABLE animals add column owner_id int NULL;
 
-ALTER TABLE
-public.animals
-ADD COLUMN IF NOT EXISTS owner_id INT REFERENCES public.owners(id);
+ALTER TABLE animals ADD CONSTRAINT animals_fk_1 FOREIGN KEY (species_id) REFERENCES species(id);
+ALTER TABLE animals ADD CONSTRAINT animals_fk_2 FOREIGN KEY (owner_id) REFERENCES owners(id);
 
 -- Fourth Milestone
 
-CREATE TABLE public.vets (
+CREATE TABLE vets (
     id int NOT NULL GENERATED ALWAYS AS IDENTITY,
     "name" varchar(100) NULL,
     age int NULL,
@@ -55,18 +53,18 @@ CREATE TABLE public.vets (
     CONSTRAINT vets_pk PRIMARY KEY (id)
 );
 
-CREATE TABLE public.specializations (
+CREATE TABLE specializations (
     vet_id int NOT NULL,
     species_id int NOT NULL,
     CONSTRAINT specializations_pk PRIMARY KEY (vet_id,species_id),
-    CONSTRAINT specializations_fk FOREIGN KEY (vet_id) REFERENCES public.vets(id) ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT specializations_fk_1 FOREIGN KEY (species_id) REFERENCES public.species(id) ON DELETE RESTRICT ON UPDATE CASCADE
+    CONSTRAINT specializations_fk FOREIGN KEY (vet_id) REFERENCES vets(id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT specializations_fk_1 FOREIGN KEY (species_id) REFERENCES species(id) ON DELETE RESTRICT ON UPDATE CASCADE
 );
 
-CREATE TABLE public.visits (
+CREATE TABLE visits (
     "date_of_visits" date NULL,
     vet_id int NULL,
     animal_id int NULL,
-    CONSTRAINT visits_fk_1 FOREIGN KEY (vet_id) REFERENCES public.vets(id) ON DELETE CASCADE ON UPDATE CASCADE,
-    CONSTRAINT visits_fk FOREIGN KEY (animal_id) REFERENCES public.animals(id) ON DELETE CASCADE ON UPDATE CASCADE
-); 
+    CONSTRAINT visits_fk_1 FOREIGN KEY (vet_id) REFERENCES vets(id) ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT visits_fk FOREIGN KEY (animal_id) REFERENCES animals(id) ON DELETE CASCADE ON UPDATE CASCADE
+);
